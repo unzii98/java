e0f7e24b566698c4598022df2a794dcff0b8bd67
Unzila98 <156695247+unzii98@users.noreply.github.com>
2024-05-17 13:59:41 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+///alert
+alert("Welcome to Our Web")
+alert("Hope you are fine and visit our website")
+alert("Welcome to js land\n Happy coding\n")
+
+
+/////variables
+var myName = "Your Full Name";
+var message;
+message = "Hello World";
+alert(message);
+// Declare variables for student's bio data
+var studentName = "Unzi";
+var studentAge = 20;
+var studentCourse = "Computer Science";
+var studentUniversity = "ABCUniversity";
+
+// Display the student's bio data in alert boxes
+alert("Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Course: " + studentCourse);
+alert("University: " + studentUniversity);
+
+
+
+//5 
+ alert('PIzza \npizz \nPIZ \nPI \nP ');
+ ///for Email 
+ alert(' MY email address is sarahu@gmail.com')
+
+
+ ///book display 
+  alert(' I am trying to learn javascript from the book \n A smarter way to learn javascript\n')
+  ///write
+  document.write(' Yah i can waitre hTML content using javascript')
+///special
+var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+        alert(specialString);
+       
+
+
+
+       
+
+        
+
+
+
